3639bed8965bee8ff7d0ea779be8ea56071a7fee
Иван Устинов <sakis2802@gmail.com>
2021-06-01 23:00:12 +0300

    initial commit

---
+++ Source/EducationGame/EducationGameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "EducationGameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class EDUCATIONGAME_API AEducationGameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
